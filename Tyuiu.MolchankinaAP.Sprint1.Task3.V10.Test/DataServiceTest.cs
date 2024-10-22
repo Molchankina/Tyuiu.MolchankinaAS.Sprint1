2959f90e0bb52f43c3360a433e5e82ca75272c6e
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-10-22 09:16:25 +0500

    Task3

---
+++ Tyuiu.MolchankinaAP.Sprint1.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.MolchankinaAP.Sprint1.Task3.V10.Lib;
+namespace Tyuiu.MolchankinaAP.Sprint1.Task3.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 23.4;
+            var res = ds.NumberToMoney(x);
+            double a = 40;
+            Assert.AreEqual(x, res);
+        }
+    }
+}
