2b5701b874cf9bf7f2bcc34f8e7d9a52c7a5aa84
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-10-22 09:25:23 +0500

    Task3

---
+++ Tyuiu.MolchankinaAS.Sprint1.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.MolchankinaAP.Sprint1.Task3.V10.Lib
+{
+    public class DataService : ISprint1Task3V10
+    {
+        public double NumberToMoney(double number)
+        {
+            double r;
+            r = Math.Floor(number);
+            double k;
+            k = (number - Math.Floor(number));
+            return r + k;
+        }
+    }
+}
