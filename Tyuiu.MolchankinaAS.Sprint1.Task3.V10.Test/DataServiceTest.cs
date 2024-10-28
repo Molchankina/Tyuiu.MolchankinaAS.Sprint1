9a34ffcbbac9d684329f5aa509bd22ce3b989155
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-10-29 00:18:50 +0500

    Task3

--- Tyuiu.MolchankinaAS.Sprint1.Task3.V10.Test/DataServiceTest.cs
+++ Tyuiu.MolchankinaAS.Sprint1.Task3.V10.Test/DataServiceTest.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.MolchankinaAS.Sprint1.Task3.V10.Test
         {
             DataService ds = new DataService();
             double number = 23.6;
-            string expectedResult = "23,6 руб. Ч это 23 руб. 60 коп.";
+            string expectedResult = "23.6 руб. Ч это 23 руб. 60 коп.";
             string actualResult = ds.NumberToMoney(number);
 
             Assert.AreEqual(expectedResult, actualResult);
